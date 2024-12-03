ff75492f1863ed8794fcbdb90c693f193b2fb0bc
rhrits <hritik.hritiksingh@gmail.com>
2024-12-03 17:20:59 +0530

    1st logo screen setup

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,14 +1,13 @@
+
 import React from 'react';
 
 const Logo = () => {
   return (
-    <div className="flex justify-center items-center bg-transparent">
-      <img src="/logo.png" alt="Wallet Logo" className="w-12" /> 
-      {/* <img src="/logo.png" alt="Wallet Logo" style={{ width: '3rem' }} /> */}
-
-      
+    <div className="flex justify-center items-center bg-transparent mt-[-80px]">
+      <p className="text-4xl font-bold bg-gradient-to-r from-yellow-400 via-yellow-300 to-yellow-600 bg-clip-text text-transparent">
+        Polink
+      </p>
     </div>
-    
   );
 };
 
