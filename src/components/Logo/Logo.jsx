5d697dd2ccdde1592b7155e6c56c3506f897c095
rhrits <hritik.hritiksingh@gmail.com>
2024-12-03 17:53:54 +0530

    Polink Animation

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,12 +1,12 @@
-
 import React from 'react';
 
 const Logo = () => {
   return (
-    <div className="flex justify-center items-center bg-transparent mt-[-80px]">
-      <p className="text-4xl font-bold bg-gradient-to-r from-yellow-400 via-yellow-300 to-yellow-600 bg-clip-text text-transparent">
-        Polink
-      </p>
+    <div className="flex justify-center items-center bg-transparent">
+      <p className="animate-ping text-4xl font-bold bg-gradient-to-r from-yellow-400 via-yellow-300 to-yellow-600 bg-clip-text text-transparent">
+  Polink
+</p>
+
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'], // Ensure this points to where your JSX/TSX files are located
   theme: {
-    extend: {},
-    
+    extend: {
+      animation: {
+        'slide-left-to-right': 'slideLeftRight 2s ease-in-out infinite',
+      },
+      keyframes: {
+        slideLeftRight: {
+          '0%': { transform: 'translateX(-100%)' },
+          '50%': { transform: 'translateX(50%)' },
+          '100%': { transform: 'translateX(0%)' },
+        },
+      },
+    },
   },
   plugins: [],
-}
-
+};
