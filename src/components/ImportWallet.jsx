ef8e43aeabe0d5be66ad6d3cb49e831224784dd3
rhrits <hritik.hritiksingh@gmail.com>
2024-12-05 12:46:31 +0530

    import wallet input

--- src/components/ImportWallet.jsx
+++ src/components/ImportWallet.jsx
@@ -30,24 +30,32 @@ const ImportWallet = () => {
   };
 
   return (
-    <div className="min-h bg-transparent text-white flex flex-col items-center p-32">
+    <div className="min-h bg-transparent text-white flex flex-col ">
       {/* Top Section */}
-      <div className="w-full flex items-center justify-between mb-6">
+      <div className="w-full flex items-center justify-between mb-6 py-6">
         <button
           onClick={() => navigate(-1)} // Go back to the previous page
           className="flex items-center text-yellow-400 font-medium hover:underline"
         >
           <img src="/arrow-left.svg" alt="Go Back" className="w-5 h-5 mr-2" />
-          Go Back
+         
         </button>
-        <h1 className="text-xl font-semibold text-center flex-grow">
+        <h1 className="text-xl font-semibold text-center flex-grow ">
           Import Wallet
         </h1>
         <div className="w-12" />
       </div>
 
       {/* Input Section */}
+
+      <h2 className="text-2xs font-normal text-left flex-grow">
+          Input
+        </h2>
+
+
       <div className="flex flex-col items-center mt-12 w-64 max-w-md">
+        
+      
         <label htmlFor="mnemonic-input" className="sr-only">
           Enter your keys
         </label>
