082833f839fe31f3107303fc58dbe748242666a0
rhrits <hritik.hritiksingh@gmail.com>
2024-12-06 17:22:52 +0530

    error message added

--- public/background.js
+++ public/background.js
@@ -9,3 +9,20 @@
 //     sendResponse({ status: "Message received successfully" });
 //   });
   
+
+
+// background.js
+
+chrome.runtime.onInstalled.addListener(() => {
+    console.log("Extension installed and service worker started.");
+  });
+  
+  // Example of using alarms or tabs API in background.js
+  chrome.alarms.create('myAlarm', { periodInMinutes: 1 });
+  
+  chrome.alarms.onAlarm.addListener((alarm) => {
+    if (alarm.name === 'myAlarm') {
+      console.log("Alarm triggered!");
+    }
+  });
+  
