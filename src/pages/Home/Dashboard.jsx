ba11bee65a6bf8e1bd4e66e8ca6915660894408d
rhrits <hritik.hritiksingh@gmail.com>
2024-12-11 17:42:43 +0530

    dashboard

--- src/pages/Home/Dashboard.jsx
+++ src/pages/Home/Dashboard.jsx
@@ -4,27 +4,25 @@ const Dashboard = () => {
     return (
         <div className="w-[350px] h-[600px] text-white">
             {/* Header Section */}
-            <div className="bg-gradient-to-b from-[#ffff] to-[#FFDF84] w-[350px] h-[8vh] text-white rounded-b-3xl flex items-center justify-between p-6">
+            <div className="bg-gradient-to-b from-[#ffff] to-[#FFDF84] w-[350px] h-[14vh] text-white rounded-b-3xl items-center justify-between p-6">
                 <div className="flex items-center space-x-4">
                     <img src="/path-to-avatar.png" alt="User Avatar" className="w-12 h-12 rounded-full" />
                     <div>
                         <h1 className="text-2xl text-black font-semibold">Hi, Hritik</h1>
                         <p className="text-sm text-black">Mainnet</p>
-                    </div>
-
-                    
+                    </div>  
                 </div>
-                
-                
-            </div>
-            <div className="flex items-center px-16 space-x-4">
-                    <p className="px-16">Total POX</p>
+            <div className="items-center px-12">
+                    <p className="px-16 text-black">Total POX</p>
             </div>
 
             <div className="flex">
-            <p className="px-16 text-xl">80.308032</p>
-            <p className="text-xs py-2">$10.45</p>
+            <p className="px-16 text-2xl text-black">80.308032</p>
+            <p className="text-xs py-2 text-black">$10.45</p>
             </div>
+                
+            </div>
+            
                     
             
 
@@ -47,7 +45,7 @@ const Dashboard = () => {
             </div>
 
             {/* Action Buttons Section */}
-            <div className="grid grid-cols-4 gap-3 p-4">
+            <div className="grid grid-cols-4 gap-2 p-4 px-[24px]">
                 <button className="bg-gradient-to-r from-yellow-400 to-red-200 p-4 rounded-xl text-black hover:brightness-110">
                     <span className="block text-xs text-center">Send</span>
                 </button>
@@ -63,7 +61,7 @@ const Dashboard = () => {
             </div>
 
             {/* Assets Section */}
-            <div className="p-6 space-y-4">
+            <div className="p-6 space-y-2">
                 <div className="flex justify-between items-center text-black bg-white rounded-xl px-6 py-2">
                     <div>
                         <h2 className="text-lg">POX</h2>
