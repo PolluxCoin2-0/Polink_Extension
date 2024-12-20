77a51361bcdd8f789a0613d782d3a5407e5470a4
rhrits <hritik.hritiksingh@gmail.com>
2024-12-20 15:50:53 +0530

    bs58

--- src/pages/CreateWallet/Backupmnemonic.jsx
+++ src/pages/CreateWallet/Backupmnemonic.jsx
@@ -38,10 +38,10 @@
 // //             toast.error("Password not found, please create a wallet first.");
 // //             return;
 // //         }
-        
+
 // //         // Use hashed password as salt for encryption
 // //         const encryptedMnemonic = CryptoJS.AES.encrypt(mnemonic, hashedPassword).toString();
-        
+
 // //         // Clear previous wallet data to ensure only the current wallet is stored
 // //         localStorage.removeItem("mnemonic");
 
@@ -929,12 +929,19 @@ import { BsQrCodeScan } from "react-icons/bs";
 import { toast } from "react-toastify";
 import EC from "elliptic";
 import CryptoJS from "crypto-js";
+import bs58check from 'bs58check';
+
+
+
+
 
 // Polyfill Buffer for Webpack 5+
 window.Buffer = Buffer;
 
 // Initialize elliptic curve secp256k1
 const ec = new EC.ec("secp256k1");
+
+
 
 const BackupMnemonic = () => {
   const [mnemonic] = useState(() => generateMnemonic());
@@ -946,14 +953,37 @@ const BackupMnemonic = () => {
     return walletName ? localStorage.getItem(walletName) : null;
   };
 
+
+  const hexToPoxAddress = (hexAddress) => {
+    const decodedHex = Buffer.from(hexAddress, 'hex');
+    const base58Address = bs58check.encode(decodedHex);
+    return base58Address;
+  };
+
+
+
+  // Derive a path (POX usually follows the Ethereum path, but change as needed)
+  // m/44'/195'/0'/0/0 is the standard POX path
+
+
+
   const generateKeys = (mnemonic) => {
     const seed = mnemonicToSeedSync(mnemonic);
+
     const root = ec.keyFromPrivate(seed.slice(0, 32).toString("hex")); // Generate root key pair
+
     const privateKey = root.getPrivate("hex");
     const publicKey = root.getPublic("hex").slice(2);
     const keccak256 = CryptoJS.SHA3(CryptoJS.enc.Hex.parse(publicKey), { outputLength: 256 }).toString();
     const poxAddressHex = "37" + keccak256.slice(-40); // POX address
-    return { privateKey, publicKey, poxAddressHex };
+    const poxAddressBase58 = hexToPoxAddress(poxAddressHex);
+
+    // Use a version byte that results in the desired prefix "P"
+    // const versionByte = Buffer.from([0x37]); // Example version byte
+    // const poxAddressWithVersion = Buffer.concat([versionByte, Buffer.from(poxAddressHex, 'hex')]);
+    // const poxAddressBase58 = bs58check.encode(poxAddressWithVersion); // Convert to Base58Check
+
+    return { privateKey, publicKey, poxAddressHex, poxAddressBase58 };
   };
 
   const storeEncryptedKeys = (mnemonic) => {
@@ -962,7 +992,7 @@ const BackupMnemonic = () => {
       toast.error("Password not found. Please create a wallet first.");
       return;
     }
-    const { privateKey, publicKey, poxAddressHex } = generateKeys(mnemonic);
+    const { privateKey, publicKey, poxAddressHex, poxAddressBase58 } = generateKeys(mnemonic);
     const encryptedPrivateKey = CryptoJS.AES.encrypt(privateKey, hashedPassword).toString();
     const encryptedPublicKey = CryptoJS.AES.encrypt(publicKey, hashedPassword).toString();
     const encryptedMnemonic = CryptoJS.AES.encrypt(mnemonic, hashedPassword).toString();
@@ -971,9 +1001,12 @@ const BackupMnemonic = () => {
     localStorage.setItem("encryptedPrivateKey", encryptedPrivateKey);
     localStorage.setItem("encryptedPublicKey", encryptedPublicKey);
     localStorage.setItem("poxAddress", poxAddressHex);
+    localStorage.setItem("poxAddressBase58", poxAddressBase58); // Store Base58 address
 
     toast.success("Keys securely stored!");
   };
+
+
 
   const copyToClipboard = async () => {
     try {
@@ -1008,77 +1041,77 @@ const BackupMnemonic = () => {
         Please write down the following words in the correct order.
       </p>
 
-    
 
-             {/* Mnemonic Section */}
-            <div className="flex flex-col items-center mt-4 w-full bg-[#1e1e1e] bg-opacity-90 p-4 rounded-xl border border-[#252118]">
-                <div className="grid grid-cols-3 gap-2 w-full">
-                    {mnemonic.split(" ").map((word, index) => (
-                        <div
-                            key={index}
-                            className="bg-[#29271F]  text-center p-2 rounded-md border border-[#252118] text-sm text-white"
-                        >
-                            {word}
-                        </div>
-                    ))}
-                </div>
-                <button
-                    onClick={copyToClipboard}
-                    className="mt-4 px-4 py-1 bg-[#1e1e1e] from-yellow-400 to-red-200 text-white font-semibold rounded-lg hover:brightness-110 flex items-center gap-2"
-                >
-                    <FiCopy className="text-lg text-yellow-400" />
-                    Copy Mnemonic
-                </button>
-            </div>
 
-            {/* QR Code Section */}
-
-            <button
-                onClick={() => setIsQrVisible(true)}
-                className="mt-6 px-4 py-3 bg-[#1e1e1e] bg-opacity-90 text-white font-semibold rounded-lg hover:brightness-110 flex items-center justify-center gap-2"
+      {/* Mnemonic Section */}
+      <div className="flex flex-col items-center mt-4 w-full bg-[#1e1e1e] bg-opacity-90 p-4 rounded-xl border border-[#252118]">
+        <div className="grid grid-cols-3 gap-2 w-full">
+          {mnemonic.split(" ").map((word, index) => (
+            <div
+              key={index}
+              className="bg-[#29271F]  text-center p-2 rounded-md border border-[#252118] text-sm text-white"
             >
-                <BsQrCodeScan className="text-lg text-yellow-400" />
-                Mnemonic QR Code
-            </button>
-
-
-            
-
-             {isQrVisible && (
-                <div className="fixed inset-0 bg-transparent w-full bg-opacity-80 backdrop-blur-md flex items-center justify-center">
-                    <div className="bg-[#ffff] bg-opacity-20 text-white p-6 rounded-lg relative max-w-[90%] w-[300px] text-center">
-                        <h2 className="text-lg text-black font-bold mb-4">Mnemonic QR Code</h2>
-                        <div className="flex items-center justify-center">
-                            <QRCodeCanvas
-                                value={mnemonic.split(" ").join(" ")} // Ensures proper encoding
-                                size={200}
-                                className="w-full max-w-[200px]"
-                            />
-                        </div>
-                        <button
-                            onClick={() => setIsQrVisible(false)}
-                            className="mt-4 px-4 py-2 bg-gradient-to-r w-full from-red-400 to-yellow-200 text-black font-semibold rounded-lg hover:brightness-110"
-                        >
-                            Close
-                        </button>
-                    </div>
-                </div>
-            )}
-
-            {/* Already Backup Button */}
-            <div className="flex items-center justify-center mt-[52px] w-full">
-                <button
-                    onClick={handleBackup}
-                    className="flex items-center justify-center py-3 rounded-xl w-full bg-gradient-to-r from-yellow-400 to-red-200 text-black font-semibold hover:brightness-110"
-                    style={{
-                        boxShadow:
-                            "6px 0px 8px 0px #FFFFFF40 inset, -6px 0px 8px 0px #00000040 inset",
-                    }}
-                >
-                    Already Backup
-                </button>
+              {word}
             </div>
-            
+          ))}
+        </div>
+        <button
+          onClick={copyToClipboard}
+          className="mt-4 px-4 py-1 bg-[#1e1e1e] from-yellow-400 to-red-200 text-white font-semibold rounded-lg hover:brightness-110 flex items-center gap-2"
+        >
+          <FiCopy className="text-lg text-yellow-400" />
+          Copy Mnemonic
+        </button>
+      </div>
+
+      {/* QR Code Section */}
+
+      <button
+        onClick={() => setIsQrVisible(true)}
+        className="mt-6 px-4 py-3 bg-[#1e1e1e] bg-opacity-90 text-white font-semibold rounded-lg hover:brightness-110 flex items-center justify-center gap-2"
+      >
+        <BsQrCodeScan className="text-lg text-yellow-400" />
+        Mnemonic QR Code
+      </button>
+
+
+
+
+      {isQrVisible && (
+        <div className="fixed inset-0 bg-transparent w-full bg-opacity-80 backdrop-blur-md flex items-center justify-center">
+          <div className="bg-[#ffff] bg-opacity-20 text-white p-6 rounded-lg relative max-w-[90%] w-[300px] text-center">
+            <h2 className="text-lg text-black font-bold mb-4">Mnemonic QR Code</h2>
+            <div className="flex items-center justify-center">
+              <QRCodeCanvas
+                value={mnemonic.split(" ").join(" ")} // Ensures proper encoding
+                size={200}
+                className="w-full max-w-[200px]"
+              />
+            </div>
+            <button
+              onClick={() => setIsQrVisible(false)}
+              className="mt-4 px-4 py-2 bg-gradient-to-r w-full from-red-400 to-yellow-200 text-black font-semibold rounded-lg hover:brightness-110"
+            >
+              Close
+            </button>
+          </div>
+        </div>
+      )}
+
+      {/* Already Backup Button */}
+      <div className="flex items-center justify-center mt-[52px] w-full">
+        <button
+          onClick={handleBackup}
+          className="flex items-center justify-center py-3 rounded-xl w-full bg-gradient-to-r from-yellow-400 to-red-200 text-black font-semibold hover:brightness-110"
+          style={{
+            boxShadow:
+              "6px 0px 8px 0px #FFFFFF40 inset, -6px 0px 8px 0px #00000040 inset",
+          }}
+        >
+          Already Backup
+        </button>
+      </div>
+
 
 
 
